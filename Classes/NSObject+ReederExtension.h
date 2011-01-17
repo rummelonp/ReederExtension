2bfbfed04e5ffc62f99b17da96c4efaa6c669383
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-17 15:40:27 +0900

    Add ReederExtension category.

---
+++ Classes/NSObject+ReederExtension.h
@@ -0,0 +1,11 @@
+//
+//  NSObject+ReederExtension.h
+//  ReederExtension
+//
+//  Created by Kazuya Takeshima on 11/01/17.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+@interface NSObject(ReederExtention)
+
+@end
---
+++ Classes/NSObject+ReederExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+ReederExtension.m
+//  ReederExtension
+//
+//  Created by Kazuya Takeshima on 11/01/17.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+#import "NSObject+ReederExtension.h"
+
+@implementation NSObject(ReederExtension)
+
+@end
