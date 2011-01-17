d7842a3883c768fd45c65d085b67d52beca57814
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-17 15:37:10 +0900

    Add principal class ReederExtension.

---
+++ Classes/ReederExtension.h
@@ -0,0 +1,17 @@
+//
+//  ReederExtension.h
+//  ReederExtension
+//
+//  Created by Kazuya Takeshima on 11/01/17.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ReederExtension : NSObject
+{
+}
+
++ (void)load;
+
+@end
---
+++ Classes/ReederExtension.m
@@ -0,0 +1,19 @@
+//
+//  ReederExtension.m
+//  ReederExtension
+//
+//  Created by Kazuya Takeshima on 11/01/17.
+//  Copyright 2011 Kazuya Takeshima. All rights reserved.
+//
+
+#import "ReederExtension.h"
+#import "NSObject+Swizzle.h"
+
+@implementation ReederExtension
+
++ (void)load
+{
+  NSLog(@"ReederExtension loaded.");
+}
+
+@end
